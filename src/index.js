56580234ff8cede11463f86559aa47228b420447
sampath006 <sampathkumar321reddy@gmail.com>
2022-06-19 15:01:00 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {ContextProvider} from "./context/Context"
-
+import axios from 'axios'
+axios.defaults.baseURL = 'https://shielded-river-38580.herokuapp.com/api/'
 ReactDOM.render(
   <React.StrictMode>
     {/* using context provider to get the user data */}
